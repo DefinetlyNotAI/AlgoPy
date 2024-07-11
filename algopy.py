16f9cca4fbf3d6d90a01861ea0f80b2547b8d200
DefinatlyNotAI <Nirt_12023@outlook.com>
2024-07-12 02:00:59 +0300

    Completed

--- algopy.py
+++ algopy.py
@@ -60,9 +60,29 @@ Complexities
 """
 
 # Fun Fact: Interstellar + Undertale + Deltarune + Stardew + Terraria + Minecraft = Life
-from colorlog_setup import *
 from datetime import datetime
 import os
+import colorlog
+
+# Configure colorlog for logging messages with colors
+logger = colorlog.getLogger()
+logger.setLevel(colorlog.INFO)  # Set the log level to INFO to capture all relevant logs
+
+handler = colorlog.StreamHandler()
+formatter = colorlog.ColoredFormatter(
+    "%(log_color)s%(levelname)-8s%(reset)s %(blue)s%(message)s",
+    datefmt=None,
+    reset=True,
+    log_colors={
+        "DEBUG": "cyan",
+        "INFO": "green",
+        "WARNING": "yellow",
+        "ERROR": "red",
+        "CRITICAL": "red,bg_white",
+    },
+)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 
 class Log:
