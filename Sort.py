b3fc4fe79f17d37fd23ff246e5b065901cd74786
DefinatlyNotAI <Nirt_12023@outlook.com>
2024-07-12 00:22:59 +0300

    Setting to 1 file and 1 sub-file - Testing

--- Sort.py
+++ Sort.py
@@ -5,7 +5,7 @@ Incorporates best practices for algorithm implementation, including efficient us
 Quicksort and merge sort are implemented recursively, demonstrating advanced recursion techniques and divide-and-conquer strategies.
 Selection sort, bubble sort, and insertion sort are included for educational purposes, showcasing simpler sorting mechanisms.
 Each sorting method is designed to handle arrays/lists of any size, with optimizations for performance and memory usage.
-
+If an error occurs it returns False.
 
 TIME COMPLEXITY: O(n^2) [Sort.using_selection] [Sort.using_bubble] [Sort.using_insertion]
 TIME COMPLEXITY: O(n log n) [Sort.using_quicksort] [Sort.using_merge_sort]
@@ -18,97 +18,102 @@ SPACE COMPLEXITY: O(n) [Sort.using_merge_sort]
 
 class Sort:
     @staticmethod
-    def using_selection(Array):
+    def __integer(Array):
+        return all(isinstance(item, int) for item in Array)
+
+    @staticmethod
+    def using_quick_sort(Array):
+        if not Sort.__integer(Array):
+            return False
+        if len(Array) <= 1:
+            return Array
+        pivot = Array[len(Array) // 2]
+        left = [x for x in Array if x < pivot]
+        middle = [x for x in Array if x == pivot]
+        right = [x for x in Array if x > pivot]
+        return Sort.using_quick_sort(left) + middle + Sort.using_quick_sort(right)
+
+    @staticmethod
+    def using_merge_sort(Array):
+        if not Sort.__integer(Array):
+            return False
+        if len(Array) <= 1:
+            return Array
+        mid = len(Array) // 2
+        left = Array[:mid]
+        right = Array[mid:]
+        return Sort.__merge(Sort.using_merge_sort(left), Sort.using_merge_sort(right))
+
+    @staticmethod
+    def __merge(left, right):
+        result = []
+        i = j = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[j])
+                j += 1
+        result.extend(left[i:])
+        result.extend(right[j:])
+        return result
+
+    @staticmethod
+    def using_selection_sort(Array):
+        if not Sort.__integer(Array):
+            return False
         for i in range(len(Array)):
             min_index = i
-            for j in range(i + 1, len(Array)):
-                if str(Array[j]) < str(Array[min_index]):
+            for j in range(i+1, len(Array)):
+                if Array[min_index] > Array[j]:
                     min_index = j
-
             Array[i], Array[min_index] = Array[min_index], Array[i]
         return Array
 
     @staticmethod
-    def using_bubble(Array):
+    def using_bubble_sort(Array):
+        if not Sort.__integer(Array):
+            return False
         n = len(Array)
         for i in range(n):
-            for j in range(0, n - i - 1):
-                if str(Array[j]) > str(Array[j + 1]):
-                    Array[j], Array[j + 1] = Array[j + 1], Array[j]
+            for j in range(0, n-i-1):
+                if Array[j] > Array[j+1]:
+                    Array[j], Array[j+1] = Array[j+1], Array[j]
         return Array
 
     @staticmethod
-    def using_insertion(Array):
+    def using_insertion_sort(Array):
+        if not Sort.__integer(Array):
+            return False
         for i in range(1, len(Array)):
             key = Array[i]
-            j = i - 1
-            while j >= 0 and str(key) < str(Array[j]):
-                Array[j + 1] = Array[j]
+            j = i-1
+            while j >= 0 and key < Array[j]:
+                Array[j+1] = Array[j]
                 j -= 1
-            Array[j + 1] = key
-        return Array
-
-    def using_quicksort(self, Array):
-        if len(Array) <= 1:
-            return Array
-        pivot = Array[len(Array) // 2]
-        left = [x for x in Array if str(x) < str(pivot)]
-        middle = [x for x in Array if str(x) == str(pivot)]
-        right = [x for x in Array if str(x) > str(pivot)]
-        return self.using_quicksort(left) + middle + self.using_quicksort(right)
-
-    def using_merge_sort(self, Array):
-        if len(Array) > 1:
-            mid = len(Array) // 2
-            L = Array[:mid]
-            R = Array[mid:]
-
-            self.using_merge_sort(L)
-            self.using_merge_sort(R)
-
-            i = j = k = 0
-            while i < len(L) and j < len(R):
-                if str(L[i]) < str(R[j]):
-                    Array[k] = L[i]
-                    i += 1
-                else:
-                    Array[k] = R[j]
-                    j += 1
-                k += 1
-
-            while i < len(L):
-                Array[k] = L[i]
-                i += 1
-                k += 1
-
-            while j < len(R):
-                Array[k] = R[j]
-                j += 1
-                k += 1
-
+            Array[j+1] = key
         return Array
 
 
-# Sample array containing integers and strings
+"""
+# Example usage
 sort = Sort()
-sample_array = [1, 37, 5, 78, 99, 5]
+arr = [34, 5, 7, 23, 32, 4]
+print("Original array:", arr)
 
-# Using selection sort
-print("Selection Sort:")
-print(sort.using_selection(sample_array.copy()))
+sorted_arr_quick = sort.using_quick_sort(arr.copy())
+print("Sorted using QuickSort:", sorted_arr_quick)
 
-# Using bubble sort
-print("\nBubble Sort:")
-print(sort.using_bubble(sample_array.copy()))
+sorted_arr_merge = sort.using_merge_sort(arr.copy())
+print("Sorted using MergeSort:", sorted_arr_merge)
 
-# Using insertion sort
-print("\nInsertion Sort:")
-print(sort.using_insertion(sample_array.copy()))
+sorted_arr_select = sort.using_selection_sort(arr.copy())
+print("Sorted using SelectionSort:", sorted_arr_select)
 
-# Using quicksort
-print("\nQuicksort:")
-print(sort.using_quicksort(sample_array.copy()))
+sorted_arr_bubble = sort.using_bubble_sort(arr.copy())
+print("Sorted using BubbleSort:", sorted_arr_bubble)
 
-# Using merge sort
-print("\nMerge Sort:")
-print(sort.using_merge_sort(sample_array.copy()))
+sorted_arr_insert = sort.using_insertion_sort(arr.copy())
+print("Sorted using InsertionSort:", sorted_arr_insert)
+"""
