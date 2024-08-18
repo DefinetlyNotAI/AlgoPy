2b8903dfee8ccd51bbb16069d36f46eca88eb3f8
DefinatlyNotAI <Nirt_12023@outlook.com>
2024-08-18 19:00:52 +0300

    Fixed minor bugs, redocumented algo.py and ReadMe.md, Also used `black .` to format the code

--- algopy.py
+++ algopy.py
@@ -125,16 +125,16 @@ from datetime import datetime
 
 class Log:
     def __init__(
-        self,
-        filename="Server.log",
-        use_colorlog=True,
-        DEBUG=False,
-        debug_color="cyan",
-        info_color="green",
-        warning_color="yellow",
-        error_color="red",
-        critical_color="red",
-        colorlog_fmt_parameters="%(log_color)s%(levelname)-8s%(reset)s %(blue)s%(message)s",
+            self,
+            filename="Server.log",
+            use_colorlog=True,
+            DEBUG=False,
+            debug_color="cyan",
+            info_color="green",
+            warning_color="yellow",
+            error_color="red",
+            critical_color="red",
+            colorlog_fmt_parameters="%(log_color)s%(levelname)-8s%(reset)s %(blue)s%(message)s",
     ):
         """
         Initializes a new instance of the LOG class.
@@ -1155,8 +1155,8 @@ class Validate:
             Validates American Express card numbers.
             """
             return cls.__luhn_algorithm(card_number) and (
-                str(card_number).startswith(("34", "37"))
-                and 15 <= len(str(card_number)) <= 16
+                    str(card_number).startswith(("34", "37"))
+                    and 15 <= len(str(card_number)) <= 16
             )
 
         @classmethod
@@ -1165,26 +1165,26 @@ class Validate:
             Validates China UnionPay card numbers.
             """
             return cls.__luhn_algorithm(card_number) and (
-                str(card_number).startswith(
-                    (
-                        "62",
-                        "64",
-                        "65",
-                        "66",
-                        "67",
-                        "68",
-                        "69",
-                        "92",
-                        "93",
-                        "94",
-                        "95",
-                        "96",
-                        "97",
-                        "98",
-                        "99",
+                    str(card_number).startswith(
+                        (
+                            "62",
+                            "64",
+                            "65",
+                            "66",
+                            "67",
+                            "68",
+                            "69",
+                            "92",
+                            "93",
+                            "94",
+                            "95",
+                            "96",
+                            "97",
+                            "98",
+                            "99",
+                        )
                     )
-                )
-                and 16 <= len(str(card_number))
+                    and 16 <= len(str(card_number))
             )
 
         @classmethod
@@ -1193,9 +1193,9 @@ class Validate:
             Validates Dankort card numbers.
             """
             return (
-                cls.__luhn_algorithm(card_number)
-                and str(card_number).startswith("49")
-                and 16 <= len(str(card_number))
+                    cls.__luhn_algorithm(card_number)
+                    and str(card_number).startswith("49")
+                    and 16 <= len(str(card_number))
             )
 
         @classmethod
@@ -1204,8 +1204,8 @@ class Validate:
             Validates Diners Club International card numbers.
             """
             return cls.__luhn_algorithm(card_number) and (
-                str(card_number).startswith(("36", "38"))
-                and 14 <= len(str(card_number)) <= 19
+                    str(card_number).startswith(("36", "38"))
+                    and 14 <= len(str(card_number)) <= 19
             )
 
         @classmethod
@@ -1214,44 +1214,44 @@ class Validate:
             Validates Discover card numbers.
             """
             return cls.__luhn_algorithm(card_number) and (
-                str(card_number).startswith(
-                    (
-                        "6011",
-                        "6221",
-                        "6222",
-                        "6223",
-                        "623",
-                        "624",
-                        "625",
-                        "626",
-                        "627",
-                        "628",
-                        "641",
-                        "642",
-                        "643",
-                        "644",
-                        "645",
-                        "646",
-                        "647",
-                        "648",
-                        "649",
-                        "65",
-                        "66",
-                        "67",
-                        "68",
-                        "69",
-                        "71",
-                        "72",
-                        "73",
-                        "74",
-                        "75",
-                        "76",
-                        "77",
-                        "78",
-                        "79",
+                    str(card_number).startswith(
+                        (
+                            "6011",
+                            "6221",
+                            "6222",
+                            "6223",
+                            "623",
+                            "624",
+                            "625",
+                            "626",
+                            "627",
+                            "628",
+                            "641",
+                            "642",
+                            "643",
+                            "644",
+                            "645",
+                            "646",
+                            "647",
+                            "648",
+                            "649",
+                            "65",
+                            "66",
+                            "67",
+                            "68",
+                            "69",
+                            "71",
+                            "72",
+                            "73",
+                            "74",
+                            "75",
+                            "76",
+                            "77",
+                            "78",
+                            "79",
+                        )
                     )
-                )
-                and 16 <= len(str(card_number))
+                    and 16 <= len(str(card_number))
             )
 
         @classmethod
@@ -1260,9 +1260,9 @@ class Validate:
             Validates JCB card numbers.
             """
             return (
-                cls.__luhn_algorithm(card_number)
-                and str(card_number).startswith("35")
-                and 16 <= len(str(card_number))
+                    cls.__luhn_algorithm(card_number)
+                    and str(card_number).startswith("35")
+                    and 16 <= len(str(card_number))
             )
 
         @classmethod
@@ -1271,40 +1271,40 @@ class Validate:
             Validates Maestro card numbers.
             """
             return cls.__luhn_algorithm(card_number) and (
-                str(card_number).startswith(
-                    (
-                        "50",
-                        "51",
-                        "52",
-                        "53",
-                        "54",
-                        "55",
-                        "56",
-                        "57",
-                        "58",
-                        "60",
-                        "61",
-                        "62",
-                        "63",
-                        "64",
-                        "65",
-                        "66",
-                        "67",
-                        "68",
-                        "69",
-                        "70",
-                        "71",
-                        "72",
-                        "73",
-                        "74",
-                        "75",
-                        "76",
-                        "77",
-                        "78",
-                        "79",
+                    str(card_number).startswith(
+                        (
+                            "50",
+                            "51",
+                            "52",
+                            "53",
+                            "54",
+                            "55",
+                            "56",
+                            "57",
+                            "58",
+                            "60",
+                            "61",
+                            "62",
+                            "63",
+                            "64",
+                            "65",
+                            "66",
+                            "67",
+                            "68",
+                            "69",
+                            "70",
+                            "71",
+                            "72",
+                            "73",
+                            "74",
+                            "75",
+                            "76",
+                            "77",
+                            "78",
+                            "79",
+                        )
                     )
-                )
-                and 12 <= len(str(card_number)) <= 19
+                    and 12 <= len(str(card_number)) <= 19
             )
 
         @classmethod
@@ -1313,11 +1313,11 @@ class Validate:
             Validates Mastercard card numbers.
             """
             return (
-                cls.__luhn_algorithm(card_number)
-                and str(card_number).startswith(
-                    ("51", "52", "53", "54", "55", "56", "57", "58", "59")
-                )
-                and 16 <= len(str(card_number))
+                    cls.__luhn_algorithm(card_number)
+                    and str(card_number).startswith(
+                ("51", "52", "53", "54", "55", "56", "57", "58", "59")
+            )
+                    and 16 <= len(str(card_number))
             )
 
         @classmethod
@@ -1326,9 +1326,9 @@ class Validate:
             Validates Visa card numbers.
             """
             return (
-                cls.__luhn_algorithm(card_number)
-                and str(card_number).startswith("4")
-                and 13 <= len(str(card_number)) <= 16
+                    cls.__luhn_algorithm(card_number)
+                    and str(card_number).startswith("4")
+                    and 13 <= len(str(card_number)) <= 16
             )
 
         @classmethod
@@ -1337,11 +1337,11 @@ class Validate:
             Validates Visa Electron card numbers.
             """
             return (
-                cls.__luhn_algorithm(card_number)
-                and str(card_number).startswith(
-                    ("40", "41", "42", "43", "44", "45", "46", "47", "48", "49")
-                )
-                and 16 <= len(str(card_number))
+                    cls.__luhn_algorithm(card_number)
+                    and str(card_number).startswith(
+                ("40", "41", "42", "43", "44", "45", "46", "47", "48", "49")
+            )
+                    and 16 <= len(str(card_number))
             )
 
         @classmethod
@@ -1350,9 +1350,9 @@ class Validate:
             Validates V Pay card numbers.
             """
             return (
-                cls.__luhn_algorithm(card_number)
-                and str(str(card_number)).startswith("28")
-                and 16 <= len(str(str(card_number)))
+                    cls.__luhn_algorithm(card_number)
+                    and str(str(card_number)).startswith("28")
+                    and 16 <= len(str(str(card_number)))
             )
 
         @classmethod
@@ -1535,8 +1535,8 @@ class Convert:
         num = 0
         Roman = Roman.upper()
         while i < len(Roman):
-            if i + 1 < len(Roman) and Roman[i : i + 2] in self.roman_to_numerical:
-                num += self.roman_to_numerical[Roman[i : i + 2]]
+            if i + 1 < len(Roman) and Roman[i: i + 2] in self.roman_to_numerical:
+                num += self.roman_to_numerical[Roman[i: i + 2]]
                 i += 2
             else:
                 num += self.roman_to_numerical[Roman[i]]
@@ -1706,9 +1706,9 @@ class Convert:
                 "Invalid input. Number, input_unit, and output_unit must all be provided."
             )
         if (
-            not isinstance(number, int)
-            or input_unit not in self.memory_dict
-            or output_unit not in self.memory_dict
+                not isinstance(number, int)
+                or input_unit not in self.memory_dict
+                or output_unit not in self.memory_dict
         ):
             raise Exception(
                 f"Invalid input. Number must be an integer, and both units must be one of the following units: \n    {str(self.memory_dict.keys()).removeprefix('dict_keys([').removesuffix('])')}."
